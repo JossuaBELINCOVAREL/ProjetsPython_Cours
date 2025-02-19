5af58f62acded80ee7aa36245a931074ae0077c1
Joko <belinjossua@gmail.com>
2025-02-19 12:06:50 +0100

    add questionnaire

---
+++ questionnaire/version1.py
@@ -0,0 +1,25 @@
+def question_reponse(numero_question, pays, a, b, c, d, bonne_reponse):
+    print(f"Question {numero_question} : Quelle est la capitale de la {pays} ? ")
+    print(f"(a) - {a}")
+    print(f"(b) - {b}")
+    print(f"(c) - {c}")
+    print(f"(d) - {d}")
+    reponse = input("Votre réponse : ")
+    resultat_reponse(reponse, bonne_reponse)
+
+def resultat_reponse(reponse, bonne_reponse):
+    if reponse == bonne_reponse:
+        print("Bonne réponse !")
+    else:
+        print(f"Mauvaise réponse... La réponse était : {bonne_reponse}")
+    print("")
+
+print("Bienvenue sur ce questionnaire ! ")
+print("")
+print("Répondez par a, b, c ou d")
+print("")
+
+reponse = question_reponse(1,"France", "Paris", "Rome", "Ankara", "Lisbonne", "a")
+reponse = question_reponse(2,"Madagscar", "Pekin", "Barcelone", "Antananarivo", "Obama", "c")
+reponse = question_reponse(3,"Italie", "Tokyo", "Barcelone", "Londres", "Rome", "d")
+reponse = question_reponse(4,"Japon", "Londres", "Tokyo", "Ankara", "Lisbonne", "b")
