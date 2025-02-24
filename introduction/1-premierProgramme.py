31cf39ddfff88b0a2c1c8d47803dc433d013722a
Joko <belinjossua@gmail.com>
2025-02-24 16:51:42 +0100

    add doc

--- introduction/1-premierProgramme.py
+++ introduction/1-premierProgramme.py
@@ -1,13 +1,14 @@
 # Comprendre les print & les variables
 
-# Premier Programme
+# Premier Programme : Déclaration et affichage de variables
 nom_personne = "admin"
 print(nom_personne)
 
+# Affichage d'une phrase avec concaténation de variables
 print("Hello World, je m'appelle " + nom_personne + " et j'ai 20ans")
 print("Hello World, c'est encore moi, " + nom_personne + "!")
 
-# Second Programme
+# Second Programme : Affichage structuré de plusieurs variables
 nom = "Dupont"
 prenom = "Jean"
 print("Prénom :")
@@ -15,7 +16,7 @@ print(prenom)
 print("Nom :")
 print(nom)
 
-# Troisième Programme
+# Troisième Programme : Introduction d'une nouvelle variable et affichage formaté
 nom = "Dupont"
 prenom = "Jean"
 profession = "Développeur"
@@ -26,11 +27,11 @@ print("Identité : " + prenom + " " + nom + " (" + profession + ")")
 
 # Demander des données à l'utilisateur
 
-# Premier Programme
+# Premier Programme : Demande du nom à l'utilisateur et affichage
 nom = input("Quel est ton nom ? ")
 print("Je m'appelle " + nom + ".")
 
-# Second Programme
+# Second Programme : Demande du nom et de l'âge à l'utilisateur et affichage
 nom_famille = input("Quel est ton nom ? ")
 age_famille = input("Quel est ton âge ? ")
 print("Je m'appelle " + nom_famille + " et j'ai " + age_famille + " ans.")
