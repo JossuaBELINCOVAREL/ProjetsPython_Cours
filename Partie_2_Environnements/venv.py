fed4968333f7a794b4b4356f2a108b4393d4ce42
Joko <belinjossua@gmail.com>
2025-03-10 13:12:44 +0100

    environnements.1

---
+++ Partie_2_Environnements/venv.py
@@ -0,0 +1,16 @@
+"""
+Pour la création d'environnements virtuels (avec GitBash): 
+
+1) Se placer dans le répertoire du projet via le terminal
+    -> cd /chemin/vers/ton/projet
+2) Créer un environnement virtuel
+    -> python -m venv venv
+3) Pour activer l'environnement
+    -> Windows : source venv/Scripts/activate
+    -> Linux/Mac : source venv/bin/activate
+4) Une fois activé, (venv) apparaîtra devant la ligne de commande, indiquant que l'environnement est bien en cours d'utilisation.
+5) Après activation, installe les dépendances nécessaires via pip
+6) Pour sortir de l'environnement 
+    -> deactivate
+
+"""
