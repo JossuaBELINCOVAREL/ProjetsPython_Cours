68e105a6a7f0fd7ea521cd46172793bc42222f79
Joko <belinjossua@gmail.com>
2025-03-10 12:55:52 +0100

    environnements

---
+++ Partie_2_Environnements/introduction.py
@@ -0,0 +1,17 @@
+"""
+Les environnepments Python:
+Permet de faire fonctionner les projets peu importe la configuration de la machine.
+Les projets dépendant de la version de Python et des modules
+Problématique : Modules et versions de Python ne sont pas compatible
+
+2 techniques pour gérer ça : 
+    -> Environnement virtuel (venv)
+    C'est une configuration isolée pour le projet
+    Le projet ne dépend plus den notre configuration pc
+    On mets les différents modules directement à l'intérieur du projet
+
+    -> Gestionnaire de configurations (pyenv)
+    Création de différents configurations sur la machine
+    Possibilité de basculer de configuration entre les projets
+
+"""
